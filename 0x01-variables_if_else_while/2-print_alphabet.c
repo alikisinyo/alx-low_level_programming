f2bc0c418095d16518c31a824673a06d672f040c
alikisinyo <56311840+alikisinyo@users.noreply.github.com>
2022-12-09 22:49:51 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
+ * Description: print all aplhabet letters
+ *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
+	char ch = 'a';
 
-	char c;
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	        putchar('\n');
-
-return (0);
+	return (0);
 }
