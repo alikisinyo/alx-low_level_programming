c9a01381cd512f554d788ddd4f9c883576ba73bb
alikisinyo <56311840+alikisinyo@users.noreply.github.com>
2022-12-09 22:47:44 +0000

    3-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
+
 /**
- * main - print alphabet in lower and uppercase
+ * main - Entry point
  *
+ * Description: print alphabet in lowercase then in uppercase
  *
- * Return: (0) Success
- */
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
+	char ch = 'a';
+	char CH = 'A';
 
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-char(ch);
-	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	/*prints a - z*/
+	while (ch <= 'z')
 	{
 		putchar(ch);
+		++ch;
 	}
-		putchar('\n');
+
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		++CH;
+	}
+	putchar('\n');
+
 	return (0);
 }
