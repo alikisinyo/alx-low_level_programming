d777ac8a9adea8ef9cd8f285cb638ea73e6dd746
alikisinyo <56311840+alikisinyo@users.noreply.github.com>
2022-12-09 22:22:30 +0000

    3-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -8,16 +8,17 @@
  */
 int main(void)
 {
-char ch;
+  char ch;
 
-for (ch = 'a' ; ch <= 'z' ; ch++)
+  for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-putchar(ch);
+  putchar(ch);
 }
-for (ch = 'A' ; ch <= 'Z' ; ch++)
+  for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-putchar(ch);
+  putchar(ch);
 }
-putchar('\n');
+  putchar('\n');
+  
 return (0);
 }
