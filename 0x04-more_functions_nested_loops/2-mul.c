8a761629eeed7c121378d6c6431f8281fd4cec28
alikisinyo <56311840+alikisinyo@users.noreply.github.com>
2022-12-15 18:48:06 +0000

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-*mul - multiplies two ints. *@a: integer. *@b: integer.
-*
-*Return: p.
-*/
-
+ *mul - multiplies two ints.
+ *@a: integer.
+ *@b: integer.
+ *
+ *Return: q.
+ */
 int mul(int a, int b)
 {
-	int p;
+	int q;
 
-	p = a * b;
-return (p);
-}
+	product = a * b;
+	return (q);
